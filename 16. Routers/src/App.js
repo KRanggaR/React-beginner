6f9f84d47a6b17b7719210e1e6ea44fbf0af39fe
Kishant Rangar <krangar83540@gmail.com>
2025-02-24 13:06:32 +0530

    router folder added

---
+++ 16. Routers/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import Blogs from "./components/Blogs";
+import Contact from "./components/Contact";
+import NoPage from "./components/NoPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
